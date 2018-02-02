28bab359d54364314fdf1858185eb2252b082c33
africanogradacac <damir_gradacac@yahoo.com>
2018-02-02 15:46:28 +0100

    gotovo

---
+++ www/js/config.js
@@ -0,0 +1,7 @@
+
+var krms_config ={			
+	'ApiUrl' : "http://www.food.arizonaonline.org/merchantapp/api",	
+	'DialogDefaultTitle' : "Food Delivery Merchant App",
+	'pushNotificationSenderid' : "440279944487",
+	'APIHasKey' : "12547874455545"
+};
